f8fd5ba782b6a228171eda244b53096af0f1303c
Igor Pereira <pereira.t.igor@gmail.com>
2020-08-26 14:48:48 -0300

    Added ECMAScript course and finished Bootstrap course.

---
+++ exercicios limpos Bootstrap/unidade09/_js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function (){
+    $("#galeria").carousel({
+        pause: "null",
+        interval: 2000,
+        wrap: true,
+        keyboard: true,
+        touch: true
+    });
+});
